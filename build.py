13484eda2c53ccb922237b9dbe70378cff71f865
jade <jadehh@live.com>
2024-05-07 00:24:36 +0800

    update auto publish script

--- build.py
+++ build.py
@@ -5,7 +5,7 @@
 # @Date     : 2024/5/6 16:39
 # @Email    : jadehh@1ive.com
 # @Software : Samples
-# @Desc     : 2024 05/06 17:20
+# @Desc     : 2024 05/07 00:24
 import os
 import argparse
 import re
